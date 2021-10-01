2adfb410277791366834a6c730e6529abdc52d4a
Maxwell Bruckhaus <maxwell.bruckhaus@gmail.com>
2021-10-01 14:01:20 -0700

    Implement delete request

--- dynamo-backend/index.js
+++ dynamo-backend/index.js
@@ -17,29 +17,41 @@ api.post('/semesters', function (request) {
 
 // Create new course
 api.post('/courses', function (request) {
-    var params = {  
-      TableName: 'courses',  
-      Item: {
-          course_id: request.body.course_id,
-          course_name: request.body.course_name,
-          course_difficulty: request.body.course_difficulty,
-          course_enjoy: request.body.course_enjoy
-      } 
-    }
-    return dynamoDb.put(params).promise(); // returns dynamo result 
+  var params = {  
+    TableName: 'courses',  
+    Item: {
+      course_id: request.body.course_id,
+      course_name: request.body.course_name,
+      course_difficulty: request.body.course_difficulty,
+      course_enjoy: request.body.course_enjoy
+    } 
+  }
+  return dynamoDb.put(params).promise(); // returns dynamo result 
 }, { success: 201 }); // returns HTTP status 201 - Created if successful
-
 
 // Get all semesters
 api.get('/semesters', function (request) {
   return dynamoDb.scan({ TableName: 'semesters' }).promise()
-      .then(response => response.Items)
+    .then(response => response.Items)
 });
 
 // Get all courses
 api.get('/courses', function (request) {
-    return dynamoDb.scan({ TableName: 'courses' }).promise()
-        .then(response => response.Items)
-  });
+  return dynamoDb.scan({ TableName: 'courses' }).promise()
+    .then(response => response.Items)
+});
+
+// Delete a course
+api.delete('/courses/{courseId}', function (request) {
+  var courseId = parseInt(request.pathParams.courseId);
+  console.log(courseId);
+  var params = {
+    TableName: 'courses',
+    Key: {
+      "course_id": courseId
+    }
+  };
+  return dynamoDb.delete(params).promise();
+}, { success: 200 });
 
 module.exports = api;
