49be91c16e0f508a9e4193c3387cdac938201bfa
Maxwell Bruckhaus <maxwell.bruckhaus@gmail.com>
2021-10-01 16:10:11 -0700

    Implement update request

--- dynamo-backend/index.js
+++ dynamo-backend/index.js
@@ -20,7 +20,6 @@ api.post('/courses', function (request) {
   var params = {  
     TableName: 'courses',  
     Item: {
-      course_id: request.body.course_id,
       course_name: request.body.course_name,
       course_difficulty: request.body.course_difficulty,
       course_enjoy: request.body.course_enjoy
@@ -42,16 +41,37 @@ api.get('/courses', function (request) {
 });
 
 // Delete a course
-api.delete('/courses/{courseId}', function (request) {
-  var courseId = parseInt(request.pathParams.courseId);
-  console.log(courseId);
+api.delete('/courses/{courseName}', function (request) {
+  var courseName = request.pathParams.courseName;
+  console.log(courseName);
   var params = {
     TableName: 'courses',
     Key: {
-      "course_id": courseId
+      "course_name": courseName
     }
   };
   return dynamoDb.delete(params).promise();
+}, { success: 200 });
+
+
+// Update a course
+api.put('/courses/{courseName}', function (request) {
+  var courseName = request.pathParams.courseName;
+  var courseDifficulty = parseInt(request.body.course_difficulty);
+  console.log(courseName);
+  console.log(courseDifficulty);
+
+  var params = {
+    TableName: 'courses',
+    Key: {
+      "course_name": courseName
+    },
+    UpdateExpression: "set course_difficulty =:newVal",
+    ExpressionAttributeValues: {
+      ":newVal": courseDifficulty
+    },
+  };
+  return dynamoDb.update(params).promise();
 }, { success: 200 });
 
 module.exports = api;
