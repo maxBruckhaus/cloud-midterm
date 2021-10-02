b56c90092eb257c08bfa35164bb253196f153e37
Maxwell Bruckhaus <maxwell.bruckhaus@gmail.com>
2021-10-01 21:51:48 -0700

    Add update button

--- react-midterm-app/src/components/CourseCreate.js
+++ react-midterm-app/src/components/CourseCreate.js
@@ -6,7 +6,8 @@ export default class CourseCreate extends React.Component {
         courses: [],
         name: '',
         difficulty: 0,
-        enjoy: false
+        enjoy: false,
+        newDifficulty: 0
     };
 
     componentDidMount() {
@@ -52,9 +53,33 @@ export default class CourseCreate extends React.Component {
     deleteCourse(courseName) {
         console.log("DELETING~!!!!!!" + courseName);
         var url = 'https://z2rmhdl2ab.execute-api.us-west-1.amazonaws.com/latest/courses/' + courseName;
-        console.log("URLEEEE1#$!#$!$");
         console.log(url);
         axios.delete(url)
+        .then(res => {
+            console.log(res);
+            axios.get('https://z2rmhdl2ab.execute-api.us-west-1.amazonaws.com/latest/courses').then(res => {
+                console.log(res);
+                this.setState({ courses: res.data });
+                window.location.reload();
+            })
+        });
+    }
+
+    updateCourse(courseName) {
+
+        var newDifficultyInt = parseInt(this.state.newDifficulty);
+
+        const updateDifficulty = {
+            course_difficulty: newDifficultyInt
+        };
+
+        var jsonDifficulty  = JSON.stringify(updateDifficulty);
+        console.log(jsonDifficulty);
+
+        console.log("UPDATING~!!!!!!" + courseName);
+        var url = 'https://z2rmhdl2ab.execute-api.us-west-1.amazonaws.com/latest/courses/' + courseName;
+        console.log(url);
+        axios.put(url, jsonDifficulty, {headers:{"Content-Type" : "application/json"}})
         .then(res => {
             console.log(res);
             axios.get('https://z2rmhdl2ab.execute-api.us-west-1.amazonaws.com/latest/courses').then(res => {
@@ -81,6 +106,8 @@ export default class CourseCreate extends React.Component {
                 <h1>Your Courses</h1>
                 {this.state.courses.map(course => 
                 <h3>Name: {course.course_name} Difficulty: {course.course_difficulty}Enjoy: {(course.course_enjoy).toString()}
+                    <form><label>New Difficulty</label><input type="number" name="newDifficulty" onChange={this.handleChange} /></form>
+                    <button onClick={() => this.updateCourse(course.course_name)}>Update Difficulty</button>
                     <button onClick={() => this.deleteCourse(course.course_name)}>Delete</button>
                 </h3>)}
 
