6187a94b41154b178e26b11f5623747bdde5b29f
Maxwell Bruckhaus <maxwell.bruckhaus@gmail.com>
2021-10-02 03:27:58 -0700

    Convert from class to functional component

--- react-midterm-app/src/components/CourseCreate.js
+++ react-midterm-app/src/components/CourseCreate.js
@@ -1,56 +1,52 @@
-import React, {Fragment} from 'react';
+import React, {useEffect, useContext, useState, Fragment} from 'react';
+import { Account, AccountContext } from "./Account";
 import axios from 'axios';
 
-export default class CourseCreate extends React.Component {
-    state = {
-        courses: [],
-        name: '',
-        difficulty: 0,
-        enjoy: false,
-        newDifficulty: 0
-    };
+export default () => {
+    const [courses, setCourses] = useState([]);
+    const [course_name, setName] = useState("");
+    const [difficulty, setDifficulty] = useState("");
+    const [enjoy, setEnjoy] = useState(true);
+    const [newDifficulty, setNewDifficulty] = useState(0);
 
-    componentDidMount() {
-        axios.get('https://z2rmhdl2ab.execute-api.us-west-1.amazonaws.com/latest/courses').then(res => {
-            console.log(res);
-            this.setState({ courses: res.data });
+    useEffect(() => {
+        axios.get('https://z2rmhdl2ab.execute-api.us-west-1.amazonaws.com/latest/courses')
+        .then(res => {
+            console.log(res)
+            setCourses(res.data)
         })
-    }
+        .catch(err => {
+            console.log(err)
+        })
+    }, [])
 
-    handleChange = event => {
-        this.setState({ 
-            [event.target.name] : event.target.value
-        });
-    };
-
-    handleSubmit = event => {
+    const handleSubmit = (event) => {
         event.preventDefault();
 
-        var difficultyInt = parseInt(this.state.difficulty);
-        var enjoyBool = this.state.enjoy == "on";
+        var course_difficulty = parseInt(difficulty);
+        var course_enjoy = enjoy == "on";
 
         const course = {
-            course_name: this.state.name,
-            course_difficulty: difficultyInt,
-            course_enjoy: enjoyBool
+            course_name,
+            course_difficulty,
+            course_enjoy
         };
 
-        var jsonCourse  = JSON.stringify(course);
+        var jsonCourse = JSON.stringify(course);
         console.log(jsonCourse);
 
         axios.post('https://z2rmhdl2ab.execute-api.us-west-1.amazonaws.com/latest/courses', jsonCourse, {headers:{"Content-Type" : "application/json"}})
         .then(res => {
-            console.log(res);
-            console.log(res.data);
+            console.log(res)
+            console.log(res.data)
             axios.get('https://z2rmhdl2ab.execute-api.us-west-1.amazonaws.com/latest/courses').then(res => {
-                console.log(res);
-                this.setState({ courses: res.data });
-                window.location.reload();
+                console.log(res)
+                setCourses(res.data)
             })
         });
     };
 
-    deleteCourse(courseName) {
+    const deleteCourse = (courseName) => {
         console.log("DELETING~!!!!!!" + courseName);
         var url = 'https://z2rmhdl2ab.execute-api.us-west-1.amazonaws.com/latest/courses/' + courseName;
         console.log(url);
@@ -59,18 +55,17 @@ export default class CourseCreate extends React.Component {
             console.log(res);
             axios.get('https://z2rmhdl2ab.execute-api.us-west-1.amazonaws.com/latest/courses').then(res => {
                 console.log(res);
-                this.setState({ courses: res.data });
-                window.location.reload();
+                setCourses(res.data)
             })
         });
     }
 
-    updateCourse(courseName) {
+    const updateCourse = (courseName) => {
 
-        var newDifficultyInt = parseInt(this.state.newDifficulty);
+        var course_difficulty = parseInt(newDifficulty);
 
         const updateDifficulty = {
-            course_difficulty: newDifficultyInt
+            course_difficulty
         };
 
         var jsonDifficulty  = JSON.stringify(updateDifficulty);
@@ -84,34 +79,30 @@ export default class CourseCreate extends React.Component {
             console.log(res);
             axios.get('https://z2rmhdl2ab.execute-api.us-west-1.amazonaws.com/latest/courses').then(res => {
                 console.log(res);
-                this.setState({ courses: res.data });
-                window.location.reload();
+                setCourses(res.data);
             })
         });
     }
 
-    render() {
-        return (
-            <Fragment>
-                <h1>Add A Course</h1>
-                <form onSubmit={this.handleSubmit}>
-                    <label>
-                        <label>Name</label><input type="text" name="name" onChange={this.handleChange} />
-                        <label>Difficulty</label><input type="number" name="difficulty" onChange={this.handleChange} />
-                        <label>Enjoy</label><input type="checkbox" name="enjoy" onChange={this.handleChange} />
-                    </label>
-                    <button type="submit">Submit</button>
-                </form>
+    return (
+        <Fragment>
+            <h1>Add A Course</h1>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    <label>Name</label><input type="text" name="name" onChange={e => setName(e.target.value)} />
+                    <label>Difficulty</label><input type="number" name="difficulty" onChange={e => setDifficulty(e.target.value)} />
+                    <label>Enjoy</label><input type="checkbox" name="enjoy" onChange={e => setEnjoy(e.target.value)} />
+                </label>
+                <button type="submit">Submit</button>
+            </form>
 
-                <h1>Your Courses</h1>
-                {this.state.courses.map(course => 
-                <h3>Name: {course.course_name} Difficulty: {course.course_difficulty}Enjoy: {(course.course_enjoy).toString()}
-                    <form><label>New Difficulty</label><input type="number" name="newDifficulty" onChange={this.handleChange} /></form>
-                    <button onClick={() => this.updateCourse(course.course_name)}>Update Difficulty</button>
-                    <button onClick={() => this.deleteCourse(course.course_name)}>Delete</button>
-                </h3>)}
-
-            </Fragment>
-        );
-    }
+            <h1>Your Courses</h1>
+            {courses.map(course => 
+            <h3>Name: {course.course_name} Difficulty: {course.course_difficulty}Enjoy: {(course.course_enjoy).toString()}
+                <form><label>New Difficulty</label><input type="number" name="newDifficulty" onChange={e => setNewDifficulty(e.target.value)} /></form>
+                <button onClick={() => updateCourse(course.course_name)}>Update Difficulty</button>
+                <button onClick={() => deleteCourse(course.course_name)}>Delete</button>
+            </h3>)}
+        </Fragment>
+    );
 }
